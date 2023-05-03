6ca4f23f1c7c9e1d11bd2b9374b10d3502262777
vholik <1viktorholik@gmail.com>
2023-05-03 10:10:10 +0200

    Getting started with Webpack

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,27 @@
+import { Configuration } from "webpack";
+import { BuildOptions } from "./types/config";
+import path from "path";
+import { buildLoaders } from "./buildLoaders";
+import { buildPlugins } from "./buildPlugins";
+import { buildResolvers } from "./buildResolvers";
+import { buildDevServer } from "./buildDevServer";
+
+export function buildWebpackConfig(options: BuildOptions): Configuration {
+  const { mode, paths, isDev } = options;
+  return {
+    mode: mode,
+    entry: paths.entry,
+    output: {
+      filename: "[name].[contenthash].js",
+      path: paths.build,
+      clean: true,
+    },
+    module: {
+      rules: buildLoaders(),
+    },
+    plugins: buildPlugins(options),
+    resolve: buildResolvers(),
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
+  };
+}
---
+++ src/component/Counter.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Counter.scss";
+
+export const Counter = () => {
+  return <div>Counter</div>;
+};
