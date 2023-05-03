ee9d021dca197f4cceab6c4117eb7960353f9cb6
vholik <1viktorholik@gmail.com>
2023-05-03 13:35:00 +0200

    Add config

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -2,9 +2,8 @@ import { FC, useMemo, useState } from "react";
 import { Theme, ThemeContext } from "./ThemeContext";
 import { LOCAL_STORAGE_THEME_KEY } from "./ThemeContext";
 
-const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as
-  | Theme
-  | Theme.LIGHT;
+const defaultTheme =
+  (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
 
 export const ThemeProvider: FC = ({ children }) => {
   const [theme, setTheme] = useState<Theme>(defaultTheme);
