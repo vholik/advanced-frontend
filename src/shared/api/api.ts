cfac912692c3a2e58f338f8181c1f239c850dec0
vholik <1viktorholik@gmail.com>
2023-06-05 13:51:11 +0200

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,7 +3,13 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage'
 
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        Authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || null,
-    },
+})
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization =
+            localStorage.getItem(USER_LOCALSTORAGE_KEY) || null
+    }
+
+    return config
 })
