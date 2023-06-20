ca9d7b5098fdd964fd29205b2d66185d4d77ef5f
vholik <1viktorholik@gmail.com>
2023-06-20 11:33:55 +0200

    skip one test, add stubs (fixtures)

--- cypress/e2e/article/article-details.cy.ts
+++ cypress/e2e/article/article-details.cy.ts
@@ -12,26 +12,30 @@ describe('User enters article details page', () => {
     afterEach(() => {
         cy.removeArticle(currentArticleId)
     })
+    describe('Api', () => {
+        it('Parsing article data', () => {
+            cy.getByTestId('ArticleDetails.Info').should('exist')
+        })
+        it('Recommendation list visible', () => {
+            cy.getByTestId('ArticleRecommendationsList').should('exist')
+        })
 
-    it('Parsing article data', () => {
-        cy.getByTestId('ArticleDetails.Info').should('exist')
+        it.skip('Leave a comment', () => {
+            cy.getByTestId('ArticleDetails.Info').should('exist')
+            cy.getByTestId('AddCommentForm').scrollIntoView()
+            cy.addComment('text')
+            cy.getByTestId('CommentCard.Content').should('exist')
+        })
     })
-    it('Recommendation list visible', () => {
-        cy.getByTestId('ArticleRecommendationsList').should('exist')
-    })
-
-    // Todo NOT WORKING
-    // it('Leave a comment', () => {
-    //     cy.getByTestId('ArticleDetails.Info').should('exist')
-    //     cy.getByTestId('AddCommentForm').scrollIntoView()
-    //     cy.addComment('text')
-    //     cy.getByTestId('CommentCard.Content').should('exist')
-    // })
-
-    it('Star rating', () => {
-        cy.getByTestId('ArticleDetails.Info').should('exist')
-        cy.getByTestId('RatingCard').scrollIntoView()
-        cy.setRate(5, 'feedback')
-        cy.get('[data-selected="true"]').should('have.length', 5)
+    describe('Api fixture', () => {
+        it('Star rating', () => {
+            cy.intercept('GET', '**/articles/*', {
+                fixture: 'article-details.json',
+            })
+            cy.getByTestId('ArticleDetails.Info').should('exist')
+            cy.getByTestId('RatingCard').scrollIntoView()
+            cy.setRate(5, 'feedback')
+            cy.get('[data-selected="true"]').should('have.length', 5)
+        })
     })
 })
--- cypress/e2e/article/articles-list.cy.ts
+++ cypress/e2e/article/articles-list.cy.ts
@@ -5,6 +5,9 @@ describe('ArticlesList', () => {
         })
     })
     it('succesfuly loaded list', () => {
+        cy.intercept('GET', '**/articles?*', {
+            fixture: 'article-details.json',
+        })
         cy.getByTestId('ArticleList').should('exist')
         cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3)
     })
